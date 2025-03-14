2dc1374af7d5c0ba096a6e822ecdb0532eba05f4
Ty Widell <tywidell@gmail.com>
2025-03-13 20:18:10 -0500

    Adding getters, setters, initial conditions

--- src/Truck.hpp
+++ src/Truck.hpp
@@ -1,3 +1,5 @@
+#include <random>
+
 // Enum to represent the simplified state of a mining truck
 enum class TruckState {
     MINING,
@@ -10,15 +12,52 @@ class Truck {
 
     int m_id;
     TruckState m_state;
+
+    uint32_t m_miningTime;
+    uint32_t m_unloadTime;
+    uint32_t m_travelTime;
+    uint32_t m_tripsCompleted;
+
+    bool m_isUnloading;
+    bool m_isTravellingToSite;
+    bool m_isTravellingToStation;
+    bool m_isMining;
+
     
 public:
-    Truck(int truckId) : m_id(truckId) {
+    Truck(int truckId) : m_id(truckId), 
+        m_state(TruckState::TRAVELING_TO_SITE), 
+        m_miningTime(0), 
+        m_unloadTime(0), 
+        m_travelTime(0), 
+        m_tripsCompleted(0), 
+        m_isUnloading(false), 
+        m_isTravellingToSite(false), 
+        m_isTravellingToStation(false),
+        m_isMining(true)
+    {
         // Initialize with random mining time (1-5 hours)
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> miningDist(1.0, 5.0);
         
+        m_miningTime = miningDist(gen);
     }
 
     int getId() const {
         return m_id;
+    }
+
+    TruckState getState() const {
+        return m_state;
+    }
+
+    void setState(TruckState newState) {
+        m_state = newState;
+    }
+
+    uint32_t getMiningTime() const {
+        return m_miningTime;
     }
 
 
