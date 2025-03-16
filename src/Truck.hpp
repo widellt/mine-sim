0b05d2c547968fb728b95bc03a2fdb7a134a6517
Ty Widell <tywidell@gmail.com>
2025-03-15 20:59:04 -0500

    First real step of simulation complete: simulate just trucks traveling, mining, and offloading without worrying about unload stations

--- src/Truck.hpp
+++ src/Truck.hpp
@@ -12,37 +12,35 @@ enum class TruckState {
 class Truck {
 
     int m_id;
+    float m_dt;
     TruckState m_state;
 
-    uint32_t m_miningTime;
-    uint32_t m_unloadTime;
-    uint32_t m_travelTime;
+    float m_miningTimeLeft;
+    float m_miningTimeTotal = 0;
+    float m_unloadTimeLeft;
+    float m_unloadTimeTotal = 0;
+    float m_travelTimeLeft;
+    float m_travelTimeTotal = 0;
     uint32_t m_tripsCompleted;
 
     bool m_isUnloading;
-    bool m_isTravellingToSite;
-    bool m_isTravellingToStation;
+    bool m_isTravelingToSite;
+    bool m_isTravelingToStation;
     bool m_isMining;
 
     
 public:
-    Truck(int truckId) : m_id(truckId), 
-        m_state(TruckState::TRAVELING_TO_SITE), 
-        m_miningTime(0), 
-        m_unloadTime(0), 
-        m_travelTime(0), 
-        m_tripsCompleted(0), 
+    Truck(int truckId, float dt) : 
+        m_id(truckId), 
+        m_dt(dt),
+        m_state(TruckState::MINING), 
         m_isUnloading(false), 
-        m_isTravellingToSite(false), 
-        m_isTravellingToStation(false),
+        m_isTravelingToSite(false), 
+        m_isTravelingToStation(false),
         m_isMining(true)
     {
         // Initialize with random mining time (1-5 hours)
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_real_distribution<> miningDist(1.0, 5.0);
-        
-        m_miningTime = miningDist(gen);
+        m_miningTimeLeft = getRandomMiningTime();
     }
 
     int getId() const {
@@ -57,8 +55,71 @@ public:
         m_state = newState;
     }
 
-    uint32_t getMiningTime() const {
-        return m_miningTime;
+    float getMiningTimeTotal() const {
+        return m_miningTimeTotal;
+    }
+
+    float getUnloadTimeTotal() const {
+        return m_unloadTimeTotal;
+    }
+
+    float getTravelTimeTotal() const {
+        return m_travelTimeTotal;
+    }
+
+    float getRandomMiningTime() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> miningDist(1.0, 5.0);
+
+        return (miningDist(gen) * 3600.0f) / m_dt;
+    }
+
+    void updateState() {
+        // Update the state of the truck based on its current state and reset random mining time
+
+        switch(m_state) {
+            case TruckState::MINING:
+                m_miningTimeLeft -= m_dt;
+                m_miningTimeTotal += m_dt;
+                if (m_miningTimeLeft <= 0) {
+                    m_state = TruckState::TRAVELING_TO_STATION;
+                    m_isMining = false;
+                    m_isTravelingToStation = true;
+                    m_miningTimeLeft = getRandomMiningTime();
+                    m_travelTimeLeft = 30.0f;
+                }
+                break;
+            case TruckState::TRAVELING_TO_STATION:
+                m_travelTimeLeft -= m_dt;
+                m_travelTimeTotal += m_dt;
+                if (m_travelTimeLeft <= 0) {
+                    m_state = TruckState::UNLOADING;
+                    m_isTravelingToStation = false;
+                    m_isUnloading = true;
+                    m_unloadTimeLeft = 5.0f;
+                }
+                break;
+            case TruckState::UNLOADING:
+                m_unloadTimeLeft -= m_dt;
+                m_unloadTimeTotal += m_dt;
+                if (m_unloadTimeLeft <= 0) {
+                    m_state = TruckState::TRAVELING_TO_SITE;
+                    m_isUnloading = false;
+                    m_isTravelingToSite = true;
+                    m_travelTimeLeft = 30.0f;
+                }
+                break;
+            case TruckState::TRAVELING_TO_SITE:
+                m_travelTimeLeft -= m_dt;
+                m_travelTimeTotal += m_dt;
+                if (m_travelTimeLeft <= 0) {
+                    m_state = TruckState::MINING;
+                    m_isTravelingToSite = false;
+                    m_isMining = true;
+                }    
+                break;
+        }
     }
 
 
--- src/TruckSim.hpp
+++ src/TruckSim.hpp
@@ -12,19 +12,24 @@ class TruckSim {
         std::vector<Truck> m_trucks;
         std::vector<Station> m_unloadStations;
         std::queue<Truck> m_waitingLine;
+        int m_numTrucks;
+        int m_numStations;
         double m_currTime = 0;
         static constexpr float TRAVEL_TIME = 1800.0f; //s, 30 min to mining site from unload station
         static constexpr float UNLOAD_TIME = 300.0f; //s, 5 min to unload
-        static constexpr float SIMULATION_DURATION = 259200.0f;
-        static constexpr float dt = 0.1f; //s
+        static constexpr float SIMULATION_DURATION = 20000.0f;
+        static constexpr float dt = 1.0f; //s
 
     public:
-        TruckSim(int numTrucks, int numStations) {
+        TruckSim(int numTrucks, int numStations) : 
+            m_numTrucks(numTrucks),
+            m_numStations(numStations) 
+        {
 
 
             // Initialize trucks and unload stations
             for (int i = 0; i < numTrucks; i++){
-                m_trucks.push_back(Truck(i));
+                m_trucks.push_back(Truck(i, dt));
             }
 
             for (int i = 0; i < numStations; i++){
@@ -35,16 +40,28 @@ class TruckSim {
 
         void simulate(){
 
+            std::cout << "Simulating " << m_numTrucks << " trucks" << std::endl;
             // Continue in while loop until we reach end time
             while (m_currTime < SIMULATION_DURATION){
 
                 m_currTime+=dt;
-
                 // Process all trucks in simulation
-                // m_trucks->updateState(dt);
-
+                for (int i = 0; i < m_numTrucks; i++){
+                    m_trucks[i].updateState();
+                }
             }
+            printTruckStats();
+        }
 
+        void printTruckStats(){
+            std::cout << "Truck Stats:" << std::endl;
+            for (int i = 0; i < m_numTrucks; i++){
+                std::cout << "Truck ID " << m_trucks[i].getId() << std::endl;
+                std::cout << "Mining Time Total: " << m_trucks[i].getMiningTimeTotal() << std::endl;
+                std::cout << "Unload Time Total: " << m_trucks[i].getUnloadTimeTotal() << std::endl;
+                std::cout << "Travel Time Total: " << m_trucks[i].getTravelTimeTotal() << std::endl;
+                std::cout << std::endl;
+            }
         }
 
 };
